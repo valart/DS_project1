87ed6b052d6c8e20f106dedcac6efe818f326c79
Artjom Valdas <artjom.valdas@ut.ee>
2022-04-01 13:36:19 +0300

    Changes by lastovko

--- algorithm.py
+++ algorithm.py
@@ -6,66 +6,14 @@ from rpyc.utils.server import ThreadedServer
 import datetime
 import _thread
 import time
-import enum
 import random
 
+from StateEnum import State
+from CriticalSectionClass import CriticalSection
+from ProcessClass import Process
+from node import Node
+
 processes = []
-
-
-class State(enum.Enum):
-    HELD = 'HELD'
-    WANTED = 'WANTED'
-    DO_NOT_WANT = 'DO-NOT-WANT'
-
-
-class CriticalSection:
-
-    def give_access(self, process):
-        _thread.start_new_thread(self.run, (process, ))
-
-    def run(self, process):
-        process.state = State.HELD
-        time.sleep(random.randint(10, process.critical_section_time_upper))
-        process.state = State.DO_NOT_WANT
-
-class Process:
-    def __init__(self, id, state, request_delay_time_upper, critical_section_time_upper):
-        self.id = id
-        self.state = state
-        self.request_delay_time_upper = request_delay_time_upper
-        self.critical_section_time_upper = critical_section_time_upper
-        self.request_timestamp = None
-
-    def start(self):
-        _thread.start_new_thread(self.run, ())
-
-    def run(self):
-        while True:
-            delay = random.randint(5, self.request_delay_time_upper)
-            time.sleep(delay)
-            if self.state == State.DO_NOT_WANT:
-                self.state = State.WANTED
-                self.request_timestamp = time.time()
-                self.request_access(processes)
-
-    def request_access(self, processes):
-        process_held = None
-
-        timestamp_min = time.time()
-        process_min = self
-
-        for process in processes:
-            if process.state == State.HELD:
-                process_held = process
-            elif process.state == State.WANTED:
-                if process.request_timestamp < timestamp_min:
-                    timestamp_min = process.request_timestamp
-                    process_min = process
-
-        if process_held is None:
-            cs = CriticalSection()
-            cs.give_access(process_min)
-
 
 
 def generate_processes(N):
